562f59f53b2f5fd8cf6ac861d6eb3778856841eb
Rohan Rathod <rohan.rthd.58@gmail.com>
2023-08-14 03:01:42 +0530

    Connected Db with Prisma and Route API created

--- admin/components/modals/store-modal.tsx
+++ admin/components/modals/store-modal.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import * as z from "zod"
+import axios from "axios";
 import {useForm } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod"
 
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { useState } from "react";
 import { Modal } from "@/components/ui/modal";
 import { 
     Form, 
@@ -15,6 +17,9 @@ import {
     FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { toast } from "react-hot-toast";
+
+
 
 
 const formSchema = z.object({
@@ -25,6 +30,8 @@ const formSchema = z.object({
 export const StoreModal = () => {
     const storeModal = useStoreModal();
 
+    const [loading, setLoading] = useState(false);
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues:{
@@ -33,7 +40,17 @@ export const StoreModal = () => {
     });
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try{
+            setLoading(true);
+
+            const response = await axios.post('/api/stores',values);
+
+            toast.success("Store created.");
+        }catch (error){
+            toast.error("Something went wrong.");
+        } finally{
+            setLoading(false)
+        }
     }
 
     return(
@@ -53,7 +70,11 @@ export const StoreModal = () => {
                 <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                        <Input placeholder="MysticMakers" {...field}/>
+                        <Input 
+                            disabled={loading} 
+                            placeholder="MysticMakers" 
+                            {...field}
+                        />
                     </FormControl>
                     <FormMessage/>
                 </FormItem>
@@ -61,11 +82,12 @@ export const StoreModal = () => {
         />
         <div className="pt-6 space-x-2 flex items-center justify-end w-full">
             <Button 
+                disabled={loading}
                 variant="outline"
                 onClick={storeModal.onClose}>
                 Cancel
                 </Button>
-            <Button>Continue</Button>
+            <Button disabled={loading}>Continue</Button>
         </div>
        </form>
        </Form>
