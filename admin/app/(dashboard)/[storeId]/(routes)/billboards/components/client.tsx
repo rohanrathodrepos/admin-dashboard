95acc39836e78b6e607517373cf42cd90a98ddb4
Rohan Rathod <rohan.rthd.58@gmail.com>
2023-08-16 11:29:27 +0530

    Added API list for billboards

--- admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -9,6 +9,7 @@ import { Separator } from "@/components/ui/separator";
 import { Billboard } from "@prisma/client";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps{
     data: BillboardColumn[]
@@ -33,6 +34,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
         </div>
         <Separator/>
         <DataTable searchKey="label" columns={columns} data={data}/>
+        <Heading title="API" description="API calls for Billboards"/>
+        <Separator/>
+        <ApiList entityName="billboards" entityIdName="billboardId"/>
         </>
     )
 }
